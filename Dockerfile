546dbef710505264724ff3d261cbe438eb235563
Gizzatovamir <gizzatovamir777@gmail.com>
2022-11-26 21:53:51 +0300

    [fix] fixed dataset downloading

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,5 @@ FROM opendronemap/odm:latest
 WORKDIR /
 RUN apt-get update && \
     apt-get install -y git
-RUN git clone https://github.com/OpenDroneMap/odm_data_aukerman.git datasets
-RUN mkdir datasets/project/ && mv datasets/images datasets/project/
 
 ENTRYPOINT ["python3", "/code/run.py"]
--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-docker build . -t odm/report_creater
-docker run -ti -v /datasets:/datasets odm/report_creater --project-path /datasets project --auto-boundary --dsm
+DIR=$(pwd)/result/
+if [ -d "$DIR" ];
+then
+    echo "dir already exists"
+else
+	git clone https://github.com/OpenDroneMap/odm_data_aukerman.git result
+fi
+docker build . -t odm/report_creater --no-cache
+docker run -ti -v $(pwd)/result/:/datasets/project/ odm/report_creater --project-path /datasets project --auto-boundary --dsm
